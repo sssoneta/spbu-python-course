f90a53bd5924866aa1ea60b11ef9213f49c37640
София Хрисанкова <hrisankova.sofia.a@mail.ru>
2024-10-03 16:03:39 +0300

    commit message

--- project/my_module.py
+++ project/my_module.py
@@ -1,15 +1,18 @@
 # Operation on vectors
 
+from math import pi, acos
 def the_scalar_product_of_vectors(vector_1, vector_2):
     """
-    Функция the_scalar_product_of_vectors проверяет, имеют ли векторы одинаковую длину. Если нет, она возвращает None.
-    В противном случае, функция вычисляет скалярное произведение, перемножая соответствующие элементы векторов и суммируя результаты.
-    Параметры:
-        vector_1 (list): Первый вектор, представленный в виде списка чисел.
-        vector_2 (list): Второй вектор, представленный в виде списка чисел.
-    Возвращаемое значение:
-        int: Скалярное произведение векторов vector_1 и vector_2, если они имеют одинаковую длину.
-        None: Если векторы имеют разную длину.
+    Function the_scalar_product_of_vectors checks whether the vectors have the same length. If not, it returns None.
+    Otherwise, the function calculates the scalar product by multiplying the corresponding elements of the vectors
+    and summing the results.
+    Parameters:
+    vector_1 (list): The first vector, represented as a list of numbers.
+    vector_2 (list): The second vector, represented as a list of numbers.
+    Return value:
+    int: The scalar product of vectors vector_1 and vector_2, if they have the same length.
+    None: If the vectors have different lengths.
+
     """
     if len(vector_1) != len(vector_2):
         return None
@@ -22,42 +25,56 @@ def the_scalar_product_of_vectors(vector_1, vector_2):
 
 def vector_length(vector):
     """"
-    Функция vector_length вычисляет длину вектора, представленного в виде списка чисел.
-    Параметры:
-    vector (list): Вектор, представленный в виде списка чисел.
-    Возвращаемое значение:
-        int: Длина вектора, округленная до целого числа
-     """
+    The vector_length function calculates the length of a vector represented as a list of numbers.
+    Parameters:
+    vector (list): A vector represented as a list of numbers.
+    Return value:
+    int: The length of the vector, rounded to an integer
+
+    """
     s = 0
     for i in vector:
         s += i**2
-    return int(pow(s, 0.5))
+    return round(pow(s, 0.5), 2)
 
 
 def the_angle_between_the_vectors(vector_1, vector_2):
     """
-    Функция the_angle_between_the_vectors вычисляет угол между двумя векторами,
-    заданными в виде списков чисел, используя скалярное произведение и длины векторов.
-    Параметры:
-    vector_1 (list): Первый вектор, представленный в виде списка чисел.
-    vector_2 (list): Второй вектор, представленный в виде списка чисел.
-    Возвращаемое значение:
-    float: Угол между векторами в радианах, c точностью до двух знаков после запятой.
+    The_angle_between_the_vectors function calculates the angle between two vectors
+    given as lists of numbers using the scalar product and the lengths of the vectors.
+    Parameters:
+    vector_1 (list): The first vector, represented as a list of numbers.
+    vector_2 (list): The second vector, represented as a list of numbers.
+    Return value:
+    float: The angle between the vectors in radians, with an accuracy of two decimal places.
 
     """
     s = the_scalar_product_of_vectors(vector_1, vector_2)
-    angle = s / (vector_length(vector_1)*vector_length(vector_2))
-    return round(angle, 2)
+    angle = s / (vector_length(vector_1) * vector_length(vector_2))
+    print(s, vector_length(vector_1), vector_length(vector_2))
+    angle = acos(angle)*180/pi
+    return round(angle)
 
 
 # Operations on matrices
 
 
 def matrix_addition(matrix_1, matrix_2):
+    """
+    Adds two matrices together.
+    Args:
+    matrix_1 (list of lists): The first matrix.
+    matrix_2 (list of lists): The second matrix.
+    Returns:
+    list of lists: The sum of the two matrices, or None if the matrices have
+    different dimensions.
+    Raises:
+    TypeError: If either input is not a list of lists.
+    """
     if len(matrix_1) != len(matrix_2) or len(matrix_1[0]) != len(matrix_2[0]):
         return None
     else:
-        matrix_sum = [[0]*len(matrix_1[0]) for i in range(len(matrix_1))]
+        matrix_sum = [[0]*len(matrix_1[0]) for _ in range(len(matrix_1))]
         for i in range(len(matrix_1)):
             for j in range(len(matrix_1[0])):
                 matrix_sum[i][j] = matrix_1[i][j] + matrix_2[i][j]
@@ -65,10 +82,22 @@ def matrix_addition(matrix_1, matrix_2):
 
 
 def matrix_multiplication(matrix_1, matrix_2):
+    """
+    The matrix_multiplication function multiplies two matrices represented as nested lists.
+    Parameters:
+    matrix_1 (list of lists): The first matrix.
+    matrix_2 (list of lists): The second matrix.
+    Return value:
+    list of lists: The product of two matrices if the number of columns in the first matrix is equal to the number of rows in the second matrix.
+    None: If the number of columns in the first matrix is not equal to the number of rows in the second matrix.
+    Exceptions:
+    TypeError: Occurs if one or both of the input arguments are not lists or nested lists.
+
+    """
     if len(matrix_1[0]) != len(matrix_2):
         return None
     else:
-        matrix_mult = [[0]*len(matrix_2[0]) for i in range(len(matrix_1))]
+        matrix_mult = [[0]*len(matrix_2[0]) for _ in range(len(matrix_1))]
         for i in range(len(matrix_1)):
             for j in range(len(matrix_2[0])):
                 for r in range(len(matrix_2)):
@@ -77,10 +106,26 @@ def matrix_multiplication(matrix_1, matrix_2):
 
 
 def matrix_transposition(matrix):
-    matrix_trans = [[0]*len(matrix) for i in range(len(matrix[0]))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            matrix_trans[j][i] = matrix[i][j]
-    return matrix_trans
+    """
+    The matrix_transposition function transposes a matrix represented as a nested list.
+    Transposing a matrix means exchanging rows and columns.
+    The function passes through each element of the original matrix matrix using nested for loops.
+    For each matrix[i][j] element (row i, column j), the function assigns the value of the matrix_trans[j][i] element
+    (row j, column i). Thus, the rows and columns of the original matrix are swapped.
+    Parameters:
+    matrix (list of lists): The matrix to be transposed.
+    Return value:
+    list of lists: A transposed matrix represented as a nested list.
+    Exceptions:
+    TypeError: Occurs if the input argument is not a list or a nested list.
 
+    """
 
+    if not matrix:
+        return []
+    else:
+        matrix_trans = [[0]*len(matrix) for _ in range(len(matrix[0]))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                matrix_trans[j][i] = matrix[i][j]
+        return matrix_trans
--- project/tests.py
+++ project/tests.py
@@ -25,10 +25,62 @@ def test_3_vector_length():
     assert vector_length([-2, 0, 5]) == int(pow(29, 0.5))
 
 
+def test_1_the_angle_between_the_vectors():
+    assert the_angle_between_the_vectors([1, 1, -2], [1, 0, -1]) == 30
 
 
+def test_2_the_angle_between_the_vectors():
+    assert the_angle_between_the_vectors([2, -2, 0], [3, 0, -3]) == 60
 
 
+def test_1_matrix_addition():
+    assert matrix_addition([[1, 2], [3, 4]], [[5, 6], [7, 8]]) == [[6, 8], [10, 12]]
 
+
+def test_2_matrix_addition():
+    assert matrix_addition([[1, 2], [3, 4]], [[5, 6, 7], [8, 9, 10]]) is None
+
+
+def test_3_matrix_addition():
+    assert matrix_addition([[]], [[]]) == [[]]
+
+
+def test_4_matrix_addition():
+    assert matrix_addition([[1]], [[2]]) == [[3]]
+
+
+def test_5_matrix_addition():
+    assert matrix_addition([[-1, -2], [-3, -4]], [[1, 2], [3, 4]]) == [[0, 0], [0, 0]]
+
+
+def test_1_matrix_multiplication():
+    assert matrix_multiplication([[1, 2], [3, 4]],  [[5, 6], [7, 8]]) == [[19, 22], [43, 50]]
+
+
+def test_2_matrix_multiplication():
+    assert matrix_multiplication([[1, 2, 3], [3, 4, 1]], [[5, 6, 7], [8, 9, 10]]) is None
+
+
+def test_3_matrix_multiplication():
+    assert matrix_multiplication([[1]], [[2]]) == [[2]]
+
+
+def test_4_matrix_multiplication():
+    assert matrix_multiplication([[]], [[]]) is None
+
+def test_1_matrix_transposition():
+    assert matrix_transposition([[1, 2, 3], [4, 5, 6]]) == [[1, 4], [2, 5], [3, 6]]
+
+
+def test_2_matrix_transposition():
+    assert matrix_transposition([]) == []
+
+
+def test_3_matrix_transposition():
+    assert matrix_transposition([[1]]) == [[1]]
+
+
+def test_4_matrix_transposition():
+    assert matrix_transposition([[1, 2], [3, 4]]) == [[1, 3], [2, 4]]
 
 
